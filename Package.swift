7be219e5d28a8796ce31301d5cb9145adf15f56a
Alexey Demin <git@dnv1ex.com>
2024-10-09 12:53:10 -0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "HLSPlayer",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "HLSPlayer",
+            targets: ["HLSPlayer"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "HLSPlayer"),
+        .testTarget(
+            name: "HLSPlayerTests",
+            dependencies: ["HLSPlayer"]
+        ),
+    ]
+)
